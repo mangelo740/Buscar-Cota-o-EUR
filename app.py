a7c9c9161827fd93fc74288bd5f7721e62f030a7
Michel Angelo <62958923+mangelo740@users.noreply.github.com>
2022-09-16 20:20:02 -0300

    cotação
    cotação

---
+++ app.py
@@ -0,0 +1,53 @@
+from tkinter import N
+import requests
+import json
+import time
+import pygame
+
+
+
+# CONEXÃO COM API DE MERCADO
+requisicao = requests.get('https://economia.awesomeapi.com.br/all/EUR-BRL')
+cotacao = requisicao.json()
+
+print('-' * 35)
+print("ROBÔ - AVISA QUANDO A COTAÇÃO É BOA")
+
+
+# VARIAVEIS DE CONTROLE
+print('-' * 35)
+segundos = int(input('Em quantos segundos atualizar? '))
+meta = float(input("Cotação desejada da moeda? "))
+print('-' * 35)
+
+valor = float(cotacao['EUR']['bid'])
+
+
+# REPETIÇÃO ATÉ CHEGAR NA META
+while valor >= meta:
+    print('-' * 35)
+    print('#### Cotação do Euro ####')
+    print('-' * 35)
+    print ('Moeda: ' + cotacao['EUR']['name'])
+    print ('Data: ' + cotacao['EUR']['create_date'])
+    print('-' * 35)
+    print('Valor atual: R$' + cotacao['EUR']['bid'])
+    time.sleep(segundos)
+
+# ALARME DE A META FOR ATINGIDA
+pygame.mixer.init()
+pygame.mixer.music.load('.\sample.mp3')
+pygame.mixer.music.play()
+
+print('-' * 35)
+print(f'A sua meta de cotação foi atingida {meta} a cotação atual é {valor}') 
+print('-' * 35)
+
+
+# SAÍR DO PROGRAMA
+sair = int(input("Digite 1 para sair: "))
+
+
+if sair == 1:
+    print('Obrigado por utilizar o programa de aviso')
+
